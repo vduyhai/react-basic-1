4840303f8a61bd0e3d68e34a0d1106ac5b6c7e39
aimehai <haivd@aimesoft.com>
2024-03-11 23:33:37 +0700

    99% hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,8 @@ import {
 } from "../components/design/Hero";
 import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
+import Generating from "./Generate";
+import Notification from "./Notification";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -24,7 +26,7 @@ const Hero = () => {
       <div className=" container relative" ref={parallaxRef}>
         <div className=" relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
           <h1 className=" h1 mb-6">
-            Explore Possibilities of AI Chatting with Brainwave
+            Explore Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
             <span className=" inline-block relative">
               Brainwave{" "}
               <img
@@ -56,6 +58,12 @@ const Hero = () => {
                   width={1024}
                   height={490}
                 />
+
+                <Generating
+                  className={
+                    "absolute left-4 ring-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2"
+                  }
+                />
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
                     {heroIcons.map((icon, index) => (
@@ -64,6 +72,12 @@ const Hero = () => {
                       </li>
                     ))}
                   </ul>
+                </ScrollParallax>
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className=" hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                    title={"Notification"}
+                  />
                 </ScrollParallax>
               </div>
             </div>
@@ -81,6 +95,8 @@ const Hero = () => {
           <BackgroundCircles />
         </div>
       </div>
+
+      <BottomLine />
     </Section>
   );
 };
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -13,10 +13,10 @@ const Section = ({
       id={id}
       className={`relative ${
         customPaddings ||
-        `py-10 lg:py-16 xl:py-20 ${crosses ? " lg:py-32 xl:py-40" : ""} ${
-          className || ""
-        }`
-      }`}
+        `py-10 lg:py-16 xl:py-20 ${crosses ? " lg:py-32 xl:py-40" : ""}`
+      }
+      ${className || " "}
+      `}
     >
       {children}
 
